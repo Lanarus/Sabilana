255a881d91eb89340c3a9810ea761b532dd86032
Lanarus <ruslanasabitova0321@gmail.com>
2025-05-18 21:26:55 +0200

    Add product filtering by category and category menu

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
 ]
 
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
-def product_list(request):
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    return render(request, 'shop/product/list.html', {'products': products})
-
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(categories=category)
+    return render(request, 'shop/product/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products,
+    })
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
