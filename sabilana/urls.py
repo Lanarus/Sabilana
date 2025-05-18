3bf0470d239d87968431c1369b48901732a73cae
Lanarus <ruslanasabitova0321@gmail.com>
2025-05-18 13:04:12 +0200

    Added static/media configuration

--- sabilana/urls.py
+++ sabilana/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include  # dodaj include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),  # dodaj to
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
