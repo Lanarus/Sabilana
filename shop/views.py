fb3488e15b611a0756aeb9e29932a505f7d27913
Lanarus <ruslanasabitova0321@gmail.com>
2025-05-19 08:54:18 +0200

    Show product action buttons (Add to cart, Notify, Favorite) in product detail view

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,8 @@ from .models import Product, Category
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.objects.filter(is_archived=False)
+
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(categories=category)
